b252b0d23eefe2f4e2adf36e69dac0284ae15431
Chris-Effah <akboateng41@gmail.com>
2023-04-21 23:07:16 -0700

    “changes”

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
