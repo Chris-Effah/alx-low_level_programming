f0a782774f1b0d17556a732ca6be77436801ea74
Chris-Effah <akboateng41@gmail.com>
2023-04-22 01:50:06 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all possible combinations.
+ *
+ * Return: 0 if program executes successfully
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
