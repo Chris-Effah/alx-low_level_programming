a746fc5659fdd5dace55118371d11502b228d51d
Chris-Effah <akboateng41@gmail.com>
2023-04-25 01:31:48 -0700

    header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - c program that prints putchar function.
+ * Return: 0 if program executes successfully
+ */
+
+int main(void)
+
+{
+	char c;
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+	}
+	return (0);
+}
