0e457c6004a95688428130fc1af9179bf11b6126
Chris-Effah <akboateng41@gmail.com>
2023-04-25 13:07:43 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 59; x++)
+	for (x = 0; x <= 23; x++)
 	{
-		for (y = 0; y <= 60; y++)
+		for (y = 0; y <= 59; y++)
 		{
 			_putchar(x / 10 + '0');
 			_putchar(x / 10 + '0');
