a2424b1732033264f0529b027409a36fba7a61fb
Chris-Effah <akboateng41@gmail.com>
2023-05-08 12:31:50 -0700

    sum of two diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_diagsums - a functio that prints the sum of two diagonals
+ * @a: name of array
+ * @size: size of the array
+ * Return: Void
+ */
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		sum1 += a[x * size + x];
+		sum2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d,  %d\n", sum1,  sum2);
+}
