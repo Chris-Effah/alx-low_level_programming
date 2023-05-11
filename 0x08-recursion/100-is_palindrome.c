3d4a6e6c0404552c1d74e195af0cbc01b7c36ef1
Chris-Effah <akboateng41@gmail.com>
2023-05-11 15:06:38 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,47 @@
 #include "main.h"
+
+int check_palindrome(char *s, int i, int length);
+int _strlen_recursion(char *s);
+
 /**
-  * wildcmp - a function that compares two strings
-  * @s1:
-  * @s2:
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is a palindrome and 0 if it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @length: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_palindrome(char *s, int i, int length)
+{
+	if (*(s + i) != *(s + length - 1))
+		return (0);
+	if (i >= length)
+		return (1);
+	return (check_palindrome(s, i + 1, length - 1));
+}
