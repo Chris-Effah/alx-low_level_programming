d444e7a46c50f8acd3489b70c821f12c45f7c160
Chris-Effah <akboateng41@gmail.com>
2023-05-12 07:25:45 -0700

    static libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr -  a function that locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return:  pointer to the first occurrence of the character c in the string s,
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _abs - a function that computes the absolute value of an integer.
+ * @n: input number as an integer
+ * Return: Abosulte value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+
+	else
+	n *= -1;
+	return (n);
+}
