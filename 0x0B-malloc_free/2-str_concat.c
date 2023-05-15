a3b549fc95bef4f0bf8e4fb17dab164513ce7d04
Chris-Effah <akboateng41@gmail.com>
2023-05-15 11:46:35 -0700

    determining the length of the string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,36 +4,39 @@
  * str_concat -  a function that concatenates two strings.
  * @s1: first string
  * @s2: second string
- * Return: pointer should point to a newly allocated space in memory
+ * Return:The returned pointer should point to a newly allocated space
  */
 char *str_concat(char *s1, char *s2)
 {
 	int i, j;
-	int length = 0;
+	int len1 = 0;
+	int len2 = 0;
 	char *s1s2;
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-	while (s1[length] != '\0')
+
+	while (s1[len1] != '\0')
 	{
-		length++;
+		len1++;
 	}
-	while (s2[length] != '\0')
+	while (s2[len2] != '\0')
 	{
-		length++;
+		len2++;
 	}
-	s1s2 = malloc(length + 1 * sizeof(char));
+	s1s2 = malloc(len1 + len2 + 1 * sizeof(char));
+
 	if (s1s2 == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < length; i++)
+	for (i = 0; i < len1; i++)
 	{
 		s1s2[i] = s1[i];
 	}
-	for (j = 0; j < length; j++)
+	for (j = 0; j < len2; j++)
 	{
 		s1s2[i + j] = s2[j];
 	}
