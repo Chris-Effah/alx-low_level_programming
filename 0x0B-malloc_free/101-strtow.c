0c67b87e7f912bf4437057dc13522f0acbc5eee2
Chris-Effah <akboateng41@gmail.com>
2023-05-17 02:00:27 -0700

    again

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,11 +7,9 @@
  */
 int count_word(char *s)
 {
-	int x = 0;
-	int y = 0;
-	int i;
+	int i, x = 0, y = 0;
 
-	for (i = 0; s[i] !- '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == ' ')
 		{
@@ -26,65 +24,46 @@ int count_word(char *s)
 	return (y);
 }
 /**
-  * strtow - function that splits a string into words
+  * **strtow - function that splits a string into words
   * @str: string to be split
   * Return: pointer to an array of strings
+  * or NULL (Error)
   */
 char **strtow(char *str)
 {
-	int len = 0;
-	char *split, *new;
-	int a;
-	int b = 0;
-	if (str == NULL || str == "")
-	{
-		return (NULL);
-	}
-	while (str != 0)
-	{
-		len++;
-	}
+	int a, start, no_of_words, end, len = 0, i = 0, b = 0;
+	char **split, *new;
+
 	while (*(str + len))
-	{
 		len++;
-	}
 	no_of_words = count_word(str);
 	if (no_of_words == 0)
-	{
 		return (NULL);
-	}
-	split = malloc((no_of_words + 1) * sizeof(char*));
+
+	split = (char **) malloc((no_of_words + 1) * sizeof(char *));
 	if (split == NULL)
-	{
 		return (NULL);
-	}
+
 	for (a = 0; a <= len; a++)
 	{
-		if (str[a] == '' || str[a] == '\0')
+		if (str[a] == ' ' || str[a] == '\0')
 		{
 			if (i)
 			{
 				end = a;
-				new = malloc((i + 1) * sizeof(char));
+				new = (char *) malloc((i + 1) * sizeof(char));
 				if (new == NULL)
-				{
 					return (NULL);
-				}
 				while (start < end)
-				{
 					*new++ = str[start++];
-				}
 				*new = '\0';
+				split[b] = new - i;
+				b++;
+				i = 0;
 			}
 		}
-	}
-	split[b] = new - i;
-	b++;
-	i = 0;
-
-	else if (i++ == 0)
-	{
-		start = a;
+		else if (i++ == 0)
+			start = i;
 	}
 	split[b] = NULL;
 	return (split);
