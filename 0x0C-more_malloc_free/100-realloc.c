8f8441647d505ec5260eb7f979f70f6037c72173
Chris-Effah <akboateng41@gmail.com>
2023-05-18 06:03:28 -0700

    reallocated memory space

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int x;
 	char *ptr1, *old_ptr;
 
+	if (new_size == old_size)
+		return (ptr);
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
