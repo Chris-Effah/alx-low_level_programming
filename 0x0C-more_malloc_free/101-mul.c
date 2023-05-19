365f2675989aee842b1e33e0eb1839f503cc35da
Chris-Effah <akboateng41@gmail.com>
2023-05-18 22:56:41 -0700

    again2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,18 +7,17 @@
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
-	int i = 0;
+	int x = 0;
 
-	while (s[i])
+	while (s[x])
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (s[x] < '0' || s[x] > '9')
 			return (0);
-		i++;
+		x++;
 	}
 	return (1);
 }
@@ -26,18 +25,17 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int x = 0;
 
-	while (s[i] != '\0')
+	while (s[x] != '\0')
 	{
-		i++;
+		x++;
 	}
-	return (i);
+	return (x);
 }
 
 /**
@@ -59,7 +57,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, dig1, dig2, *res, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -67,36 +65,36 @@ int main(int argc, char *argv[])
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
+	res = malloc(sizeof(int) * len);
+	if (!res)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
+		res[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len1] - '0';
+		dig1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			dig2 = s2[len2] - '0';
+			carry += res[len1 + len2 + 1] + (dig1 * dig2);
+			res[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
+			res[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if (result[i])
+		if (res[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');
+			_putchar(res[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
-	free(result);
+	free(res);
 	return (0);
 }
 
