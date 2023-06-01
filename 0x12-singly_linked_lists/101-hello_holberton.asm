9a20b130e127b1fb9ae1bbd728f88176681b5f7b
Chris-Effah <akboateng41@gmail.com>
2023-06-01 08:56:41 -0700

    added a new line character in assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -11,12 +11,12 @@ sub rsp, 8                       ; Align the stack
 
 mov rdi, fmt                  ; set the format string
 mov rsi, msg                   ; set the address of the hello string
-xor eax, eax                     ; clear eax register
+xor rax, rax                     ; clear eax register
 call printf                      ; call printf function
 
 
 add rsp, 8                       ; restore the stack
-mov eax, 60                      ; exit system call number
+mov rax, 60                      ; exit system call number
 xor edi, edi                     ; exit status 0
 syscall                         ; invoke the system call
 
