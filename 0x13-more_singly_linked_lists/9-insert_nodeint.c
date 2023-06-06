7a5055b4faf49eb8b59fe2060e4e9b7d2493548e
Chris-Effah <akboateng41@gmail.com>
2023-06-06 13:21:46 -0700

    tried again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (x = 0; x < idx - 1; x++)
 	{
-		if (temp == NULL)
+		if (temp == NULL || temp->next == NULL)
 		{
 			free(n_node);
 			return (NULL);
