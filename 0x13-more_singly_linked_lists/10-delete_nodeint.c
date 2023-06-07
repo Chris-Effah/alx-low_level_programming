cfee08a0b820bcd255e0f6738d5a77b9254fe336
Chris-Effah <akboateng41@gmail.com>
2023-06-06 22:44:20 -0700

    5

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = *head;/*storing the current head node in the temp variable*/
 		*head = (*head)->next;
 		free(temp);
-		return (-1);
+		return (1);
 	}
 
 	pres = *head;
