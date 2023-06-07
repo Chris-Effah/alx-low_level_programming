49a5e87500e3a75bec6a2e07f63203737e899d03
Chris-Effah <akboateng41@gmail.com>
2023-06-07 15:15:01 -0700

    fixed a segmentation fault error

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-  *print_listint_safe - a function that prints out the elements of a linked
-  *list
-  *@head: head node of the linked list
-  *Return: number of nodes in the linked lists
-  */
+ *print_listint_safe - a function that prints out the elements of a linked
+ *list
+ *@head: head node of the linked list
+ *Return: number of nodes in the linked lists
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *slow, *fast;
@@ -22,17 +22,28 @@ size_t print_listint_safe(const listint_t *head)
 		printf("[%p] %d\n", (void *)slow, slow->n);/**prints the
 							    *addresses of
 							    *current node
-							   */
+							    */
 		no_of_nodes++;
 
 		slow = slow->next;
 		fast = fast->next->next;
 
 		if (slow == fast)
-		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			no_of_nodes++;
 			break;
+	}
+
+	if (slow == fast)
+	{
+		printf("-> [%p] %d\n", (void *)slow, slow->n);
+		no_of_nodes++;
+
+		slow = head;
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			no_of_nodes++;
+			slow = slow->next;
+			fast = fast->next;
 		}
 	}
 
