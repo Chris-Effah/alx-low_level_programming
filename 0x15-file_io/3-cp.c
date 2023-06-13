87b17de63cc2e2322917b66c05490a7117f59a95
Chris-Effah <akboateng41@gmail.com>
2023-06-13 11:49:44 -0700

    included an append flag

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,8 +42,7 @@ void cp_file(const char *file_from, const char *file_to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-			| S_IRGRP | S_IROTH);
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
