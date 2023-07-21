6914f38c6c18716c258a4e889e70b02cddad7b26
Chris-Effah <akboateng41@gmail.com>
2023-07-21 15:11:22 -0700

    final

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,91 +3,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-int f4(char *usrn, int len) {
-	int ch;
-	int vch;
-	unsigned int rand_num;
+int main(int argc __attribute__((unused)), char *argv[]) {
+    char key[7];
+    char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+    size_t i, len = strlen(argv[1]);
+    int add = 0;
+    int product = 1;
+    int sum_squares = 0;
+    char max_char;
 
-	ch = *usrn;
-	vch = 0;
+    key[0] = l[(len ^ 59) & 63];
 
-	while (vch < len) {
-		if (ch < usrn[vch])
-			ch = usrn[vch];
-		vch += 1;
-	}
+    for (i = 0; i < len; i++)
+        add += argv[1][i];
+    key[1] = l[(add ^ 79) & 63];
 
-	srand(ch ^ 14);
-	rand_num = rand();
+    for (i = 0; i < len; i++)
+        product *= argv[1][i];
+    key[2] = l[(product ^ 85) & 63];
 
-	return (rand_num & 63);
-}
+    max_char = argv[1][0];
+    for (i = 0; i < len; i++) {
+        if (argv[1][i] >= max_char)
+            max_char = argv[1][i];
+    }
+    srand(max_char ^ 14);
+    key[3] = l[rand() & 63];
 
-int f5(char *usrn, int len) {
-	int ch;
-	int vch;
+    for (i = 0; i < len; i++)
+        sum_squares += argv[1][i] * argv[1][i];
+    key[4] = l[(sum_squares ^ 239) & 63];
 
-	ch = vch = 0;
+    srand(max_char ^ 229);
+    while (max_char--) {
+        rand();
+    }
+    key[5] = l[(rand() ^ 229) & 63];
 
-	while (vch < len) {
-		ch = ch + usrn[vch] * usrn[vch];
-		vch += 1;
-	}
+    key[6] = '\0';
 
-	return (((unsigned int)ch ^ 239) & 63);
-}
-
-int f6(char *usrn) {
-	int ch;
-	int vch;
-
-	ch = vch = 0;
-
-	while (vch < *usrn) {
-		ch = rand();
-		vch += 1;
-	}
-
-	return (((unsigned int)ch ^ 229) & 63);
-}
-
-int main(int argc, char **argv) {
-	char keygen[7];
-	int len, ch, vch;
-	long alph[] = {
-		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
-		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
-		0x723161513346655a, 0x6b756f494b646850 };
-	(void)argc;
-
-	for (len = 0; argv[1][len]; len++)
-		;
-	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-
-	ch = vch = 0;
-	while (vch < len) {
-		ch = ch + argv[1][vch];
-		vch = vch + 1;
-	}
-	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-
-	ch = 1;
-	vch = 0;
-	while (vch < len) {
-		ch = argv[1][vch] * ch;
-		vch = vch + 1;
-	}
-	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-
-	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-
-	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-
-	keygen[5] = ((char *)alph)[f6(argv[1])];
-	keygen[6] = '\0';
-
-	for (ch = 0; keygen[ch]; ch++)
-		printf("%c", keygen[ch]);
-	return 0;
+    printf("%s\n", key);
+    return 0;
 }
 
