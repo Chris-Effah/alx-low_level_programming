1e5b3a135da8ab4c89f0b57f8d60f4ff79836429
Chris-Effah <akboateng41@gmail.com>
2023-08-11 21:18:58 -0700

    implemented the djb2 algorithm

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+  * key_index - a function to obtain the index of the key
+  * @key: a string that points to the key
+  * @size: size of the array
+  * Return: an index at which the key value pair is stored in the array
+  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value = hash_djb2(key);
+
+	return (hash_value & size);
+}
